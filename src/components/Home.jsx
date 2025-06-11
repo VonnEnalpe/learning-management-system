da8369d7ab65415a99467d9b6b6f41b7ca2a9d44
VonnEnalpe <vonnenalpe@gmail.com>
2025-06-11 20:01:29 +0800

    add files for components and services

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function Home() {
+  return <div>
+    <Button
+      title="Test"
+    >Test</Button>
+  </div>;
+}
